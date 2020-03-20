34d610fe370c18acbc63fd1dabd4fd6ff34f1bfe
Sergey Petrin <spetrin@gmail.com>
2020-03-20 15:33:47 +0300

    Add stub view

--- duties/tests.py
+++ duties/tests.py
@@ -6,6 +6,13 @@ from .models import DutyDate, Group, DutyPerson
 from .utils import generate_duties, generate_groups, generate_persons
 
 
+class TestView(TestCase):
+
+    def test_view(self):
+        client = Client()
+        self.assertEquals(client.get('/').status_code, 200)
+
+
 class TestUtils(TestCase):
 
     def test_generate_groups(self):
--- duties/views.py
+++ duties/views.py
@@ -1,3 +1,16 @@
+import datetime
+
 from django.shortcuts import render
 
-# Create your views here.
+from duties.models import DutyDate
+
+
+def index(request):
+    today = datetime.date.today()
+    duties_today = DutyDate.objects.filter(date=today).first().dutyperson_set.all()
+    context = {
+        'duty': duties_today,
+        'groups': [person.group for person in duties_today]
+    }
+
+    return render(request, 'duties/index.html', context)
