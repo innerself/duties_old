b9b6d7c4e705862e5fc37a225c0178cb976d3cfd
Sergey Petrin <spetrin@gmail.com>
2020-03-24 11:11:05 +0300

    Register models in the admin

--- duties/admin.py
+++ duties/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from duties import models
+
+admin.site.register(models.Group)
+# admin.site.register(models.DutyPerson)
+admin.site.register(models.DutyDate)
+
+
+@admin.register(models.DutyPerson)
+class DutyPersonAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'group')
+    list_filter = ('group', )
+    search_fields = ('first_name', 'last_name')
